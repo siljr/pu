57c38c0539076ce89950ade3af8fb646408b51a4
Silje Riseng <silje.riseng@gmail.com>
2017-02-05 23:01:30 +0100

    added login and logut

--- qBot/qBot/settings.py
+++ qBot/qBot/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'qBot',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# when logged in, the user will be redirected to this page
+LOGIN_REDIRECT_URL = 'questions:index'
--- qBot/qBot/urls.py
+++ qBot/qBot/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^questions/', include('questions.urls')),
+    # added by silje
+    url(r'^questions/', include('questions.urls', namespace="questions")),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page':'login'}, name='logout'),
+
     url(r'^admin/', admin.site.urls),
 ]
