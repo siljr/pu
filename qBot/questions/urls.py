bb7d2cf4950dcba7cf7115e1401f6f517e8ad7ef
Unknown <hakon.molven@gmail.com>
2017-03-02 16:56:37 +0100

    Tester filterbranch

--- qBot/questions/urls.py
+++ qBot/questions/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     # url(r'^$', views.questions, name='questions'),
     url(r'create_question', views.register_question, name='create_question'),
+    # just a comment
 ]
