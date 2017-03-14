24f28abbad612203bd5e83314dfbdf6493bb4c07
Silje Riseng <silje.riseng@gmail.com>
2017-03-14 13:10:46 +0100

    added a answer model

--- qBot/questions/models.py
+++ qBot/questions/models.py
@@ -20,3 +20,15 @@ class Question(models.Model):
         if self.created_at is None:
             self.created_at = timezone.now()
         super().save(*args, **kwargs)
+
+class Answer(models.Model):
+    body = models.TextField()
+    created_at = models.DateTimeField(null=True, blank=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, default = 1)
+    votes = models.ManyToManyField(User, related_name='voted_for_question')
+
+    # adds a timestamp to the question posted
+    def save(self, *args, **kwargs):
+        if self.created_at is None:
+            self.created_at = timezone.now()
+        super().save(*args, **kwargs)
