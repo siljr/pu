7ce0d172ab1f1e2773b1f4d82459c98c4b47dbcc
Longtth <longtth85@gmail.com>
2017-03-16 14:41:45 +0100

    Made Django tests for RegistrationApp

--- qBot/registrationApp/tests.py
+++ qBot/registrationApp/tests.py
@@ -1,7 +1,5 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
-from .forms import RegistrationForm
-
 
 
 class RegisterAppTestCase(TestCase):
@@ -9,15 +7,39 @@ class RegisterAppTestCase(TestCase):
     def test_register_respone(self):
         response = self.client.get(reverse('questions:index'), follow= True)
         # 200 = evrything ok
+        #print(response.redirect_chain)
         self.assertEqual(response.status_code, 200)
-
-    def createUser(self):
-        testUser = RegistrationForm()
-        testUser.username('long')
-        testUser.email('long@email.com')
-        testUser.password1('long')
-        testUser.password2('long')
 
     #Test to see if form restrictions work
     def test_register_user(self):
-        self.assertEqual()
+        response = self.client.post('/register/',
+                                    {'username': 'test', 'email': 'test@test.no', 'password1': 'test',
+                                     'password2': 'test'}, follow=True)
+        #print(response.redirect_chain)
+        self.assertEqual(response.status_code, 200)
+
+    # wrong password twice
+    # valid email
+    # username not taken
+    def test_register_password_error(self):
+        response = self.client.post('/register/',
+                                    {'username': 'test', 'email': 'test@test.no', 'password1': 'test',
+                                     'password2': 'test1'}, follow=True)
+        self.assertNotEqual(response.request['PATH_INFO'], 'login')
+
+    def test_register_username_error(self):
+        self.client.post('/register/',
+                                    {'username': 'test', 'email': 'test@test.no', 'password1': 'test',
+                                     'password2': 'test'}, follow=True)
+
+        response2 = self.client.post('/register/',
+                                    {'username': 'test', 'email': 'test@test.no', 'password1': 'test',
+                                     'password2': 'test'}, follow=True)
+
+        self.assertNotEqual(response2.request['PATH_INFO'], 'login')
+
+    def test_register_email_error(self):
+        response = self.client.post('/register/',
+                                    {'username': 'test', 'email': 'test@test', 'password1': 'test',
+                                     'password2': 'test1'}, follow=True)
+        self.assertNotEqual(response.request['PATH_INFO'], 'login')
