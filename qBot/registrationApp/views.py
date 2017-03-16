3b969503b2b67121faa96de7383e9b721b155714
Silje Riseng <silje.riseng@gmail.com>
2017-03-16 16:29:06 +0100

    something

--- qBot/registrationApp/views.py
+++ qBot/registrationApp/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
-from registrationApp.forms import *
+# from registrationApp.forms import *
 
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views.generic.edit import FormView
 
 
 def register_page(request):
     if request.method == 'POST':
-        form = RegistrationForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
+            print('formet fungerer')
             user = User.objects.create_user(username=form.cleaned_data['username'], password=form.cleaned_data['password1'], email=form.cleaned_data['email'])
             return HttpResponseRedirect('/questions')
-    form = RegistrationForm()
+    form = UserCreationForm()
     context = {'form': form}
     return render(request, 'registrationApp/register.html', context)
