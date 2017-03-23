dea80716f1aad690968477a9428ac34be87dffb7
Longtth <longtth85@gmail.com>
2017-03-23 16:00:15 +0100

    more migrations

--- qBot/questions/tests.py
+++ qBot/questions/tests.py
@@ -82,13 +82,20 @@ class QuestionsCreateTestCase(TestCase):
         # check if index.html is in the list of used templates
         self.assertTemplateUsed(response, 'index.html')
 
+        # get all titles
         all = Question.objects.all()
         allQ = [x.title for x in all]
+
+        # see if the right title is in the object list
         self.assertIn("tittel", allQ)
+
+        # see if the number of Question objects is correct
+        self.assertEqual(len(allQ), 2)
 
     def test_add_question(self):
         # tests to see if title and body is correct
         q1 = Question.objects.get(title = "tittel")
+
         self.assertEqual(q1.body, "ipsum lorem")
         q2 = Question.objects.get(title = "tittel2")
         self.assertEqual(q2.body, "ipsum lorem2")
