e1fc5fce6f14aa1d5fb2b23ffff51955062eb194
Unknown <hakon.molven@gmail.com>
2017-04-21 12:52:51 +0200

    Added comments on tag funcitons

--- qBot/questions/forms.py
+++ qBot/questions/forms.py
@@ -3,7 +3,7 @@ from django import forms
 class QuestionForm(forms.Form):
     title = forms.CharField(max_length=128)
     body = forms.CharField()
-    tags = forms.CharField(required=False)
+    tags = forms.CharField(required=False)  # Makes tagging optional
 
 class AnswerForm(forms.Form):
     body = forms.CharField(label='body')
